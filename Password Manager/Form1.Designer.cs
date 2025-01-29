3bf1cddc89f09deb420286cca9d5bee50926e0a1
sivav <sivav@PC-XMFKDAL>
2025-01-29 19:16:21 +0000

    Revert "Added the password manager screen where Passwords can be added and saved/retrieved with a log out button"
    This reverts commit 0cf48d5d92749deacfb19a0d9f9c0d9255cafc9a.

--- Password Manager/Form1.Designer.cs
+++ Password Manager/Form1.Designer.cs
@@ -43,56 +43,50 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(168, 9);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(134, 9);
             label1.Name = "label1";
-            label1.Size = new Size(190, 65);
+            label1.Size = new Size(347, 54);
             label1.TabIndex = 0;
-            label1.Text = "Log in";
+            label1.Text = "Welcome Back";
             label1.Click += label1_Click;
             // 
             // txtUserName
             // 
-            txtUserName.Location = new Point(293, 159);
-            txtUserName.Margin = new Padding(4);
+            txtUserName.Location = new Point(220, 257);
             txtUserName.Name = "txtUserName";
-            txtUserName.Size = new Size(284, 31);
+            txtUserName.Size = new Size(228, 27);
             txtUserName.TabIndex = 1;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(293, 217);
-            txtPassword.Margin = new Padding(4);
+            txtPassword.Location = new Point(220, 303);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(284, 31);
+            txtPassword.Size = new Size(228, 27);
             txtPassword.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(156, 163);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(110, 260);
             label2.Name = "label2";
-            label2.Size = new Size(91, 25);
+            label2.Size = new Size(75, 20);
             label2.TabIndex = 4;
             label2.Text = "Username";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(156, 220);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(110, 306);
             label3.Name = "label3";
-            label3.Size = new Size(87, 25);
+            label3.Size = new Size(70, 20);
             label3.TabIndex = 5;
             label3.Text = "Password";
             // 
             // button1
             // 
-            button1.Location = new Point(293, 296);
-            button1.Margin = new Padding(4);
+            button1.Location = new Point(220, 366);
             button1.Name = "button1";
-            button1.Size = new Size(129, 45);
+            button1.Size = new Size(103, 36);
             button1.TabIndex = 6;
             button1.Text = "Log in";
             button1.UseVisualStyleBackColor = true;
@@ -100,10 +94,9 @@
             // 
             // button2
             // 
-            button2.Location = new Point(442, 296);
-            button2.Margin = new Padding(4);
+            button2.Location = new Point(339, 366);
             button2.Name = "button2";
-            button2.Size = new Size(136, 45);
+            button2.Size = new Size(109, 36);
             button2.TabIndex = 7;
             button2.Text = "Sign up";
             button2.UseVisualStyleBackColor = true;
@@ -113,20 +106,18 @@
             // 
             label4.AutoSize = true;
             label4.ForeColor = Color.Red;
-            label4.Location = new Point(293, 254);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(220, 333);
             label4.Name = "label4";
-            label4.Size = new Size(163, 25);
+            label4.Size = new Size(134, 20);
             label4.TabIndex = 8;
             label4.Text = "Incorrect password";
             label4.Click += label4_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(684, 524);
-            button3.Margin = new Padding(4);
+            button3.Location = new Point(547, 419);
             button3.Name = "button3";
-            button3.Size = new Size(99, 36);
+            button3.Size = new Size(79, 29);
             button3.TabIndex = 9;
             button3.Text = "Exit";
             button3.UseVisualStyleBackColor = true;
@@ -134,9 +125,9 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 562);
+            ClientSize = new Size(626, 450);
             Controls.Add(button3);
             Controls.Add(label4);
             Controls.Add(button2);
@@ -146,9 +137,8 @@
             Controls.Add(txtPassword);
             Controls.Add(txtUserName);
             Controls.Add(label1);
-            Margin = new Padding(4);
             Name = "Form1";
-            Text = "Log in";
+            Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
